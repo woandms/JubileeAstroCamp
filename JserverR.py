1a22cd1186cacb9363274fa47689f06181e45d54
woandms <42778957+woandms@users.noreply.github.com>
2023-09-09 17:42:49 +0300

    Delete JserverR.py

--- JserverR.py
+++
@@ -1,8 +0,0 @@
-import http.server
-
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path in ["/index.html", "/style.css", "/script.js"]:
-            http.server.SimpleHTTPRequestHandler.do_GET(self)
-        else:
-            self.send_error(404, "File not found")
